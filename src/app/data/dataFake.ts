7bf8493a3bfd0e189c1b7fdbeaf79c881a4b9b04
BotPlayerAI <matheusomega5@gmail.com>
2024-10-29 13:05:08 -0300

    I've changed the website title from MARVEL BLOG to GAMING BLOG, updated each image, title, and description of the news, and added a link to each news article inside a 'Read More' section. Additionally, I've changed the links for LinkedIn and GitHub, and I've removed the Instagram link.

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,39 @@
+import { Title } from "@angular/platform-browser";
+
 export const dataFake = [
   {
-    "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    id: '1',
+    title: 'Fortnite is streamlining its many battle passes',
+    description:
+      'Epic Games is changing how its battle passes work so that you can progress in Lego Fortnite and Fortnite Festival with XP.',
+    photoCover:
+      'https://duet-cdn.vox-cdn.com/thumbor/0x0:1485x1004/640x427/filters:focal(743x502:744x503):format(webp)/cdn.vox-cdn.com/uploads/chorus_asset/file/25574399/Still015.jpg',
+    link: 'https://www.theverge.com/2024/10/28/24282075/fortnite-streamlining-battle-passes-music-lego-festival',
   },
   {
-    "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
-  }
-]
+    id: '2',
+    title:
+      'Actors union SAG-AFTRA signs an agreement with AI voice company Ethovox',
+    description:
+      'As reported by Game Developer, the union has signed a deal with Ethovox, which it says will make sure any actor deciding to work with Ethovox will be “protected and compensated fairly”.',
+    photoCover:
+      'https://pbs.twimg.com/media/GbD5K5yXIAANKLX?format=jpg&name=large',
+    link: 'https://www.videogameschronicle.com/news/actors-union-sag-aftra-signs-an-agreement-with-ai-voice-company-ethovox/',
+  },
+  {
+    id: '3',
+    title: 'Indiana Jones and the Great Circle: The Final Preview',
+    description: 'Better put a Great Circle around December 9 on your calendar',
+    photoCover: 'https://i.ytimg.com/vi/Pf7PLN13j7Y/maxresdefault.jpg',
+    link: 'https://www.ign.com/articles/indiana-jones-and-the-great-circle-the-final-preview',
+  },
+  {
+    id: '4',
+    title:
+      'Oregon Trail is the next game getting a blockbuster movie adaptation',
+    description: 'It goes Oregon and on',
+    photoCover:
+      'https://assetsio.gnwcdn.com/th-2403231306?width=690&quality=75&format=jpg&auto=webp',
+    link: 'https://www.eurogamer.net/oregon-trail-is-the-next-game-getting-a-blockbuster-movie-adaptation',
+  },
+];
